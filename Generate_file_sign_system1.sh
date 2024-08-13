c7329e6db16dd466ab15da6e5997d4ccebcbe368
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-08-13 12:13:02 +0545

    Create Generate_file_sign_system1.sh

---
+++ Generate_file_sign_system1.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "Write the detail inside the file:"
+
+read -r custom_data
+
+
+echo "Generating file with custom data..."
+
+echo "$custom_data" > dailyfile.txt
+
+echo "File generated at $(date)."
+
+
+echo "Encrypting the file..."
+
+openssl pkeyutl -encrypt -inkey makiikey.pub -pubin -in dailyfile.txt -out dailyfile.txt.enc
+
+if [ $? -eq 0 ]; then
+
+    echo "File encrypted."
+
+else
+
+    echo "Encryption failed."
+
+    exit 1
+
+fi
+
+
+echo "Creating digital signature..."
+
+openssl dgst -sha256 -sign denishkey.pem -out dailyfile.sig dailyfile.txt
+
+if [ $? -eq 0 ]; then
+
+    echo "Digital signature created."
+
+else
+
+    echo "Failed."
+
+    exit 1
+
+fi
+
+
+echo "Transferring files to makii..."
+
+scp dailyfile.txt.enc dailyfile.sig makii@192.168.56.103:~/Desktop/system2/
+
+if [ $? -eq 0 ]; then
+
+    echo "Transfer complete."
+
+else
+
+    echo "Transfer failed."
+
+    exit 1
+
+fi
+
